8b9b79afd9beafd14e76ce136bca0fca809fb09f
James Cracknell <james.h.cracknell@gmail.com>
2013-08-25 11:57:54 -0600

    Added Repetition parser

--- smd/src/main/scala/parsing/Parsers.scala
+++ smd/src/main/scala/parsing/Parsers.scala
@@ -95,6 +95,16 @@ trait Parser[+A] extends (ParsingContext => ParsingResult[A]) {
   def ^^[B](transform: ParsingResult[A] => B): Parser[B] = Transform(this, transform)
 
   def ? : Parser[Option[A]] = Optional(this)
+
+  def *                      : Parser[Seq[A]] = Repetition(this, None, None)
+  def *   (occurs: Int)      : Parser[Seq[A]] = Repetition(this, Some(occurs), Some(occurs))
+  def *   (min: Int, max:Int): Parser[Seq[A]] = Repetition(this, Some(min), Some(max))
+  def *   (range: (Int, Int)): Parser[Seq[A]] = Repetition(this, Some(range._1), Some(range._2))
+  def *>  (min: Int)         : Parser[Seq[A]] = this*>=(min + 1)
+  def *>= (min: Int)         : Parser[Seq[A]] = Repetition(this, Some(min), None)
+  def *<  (max: Int)         : Parser[Seq[A]] = this*<=(max - 1)
+  def *<= (max: Int)         : Parser[Seq[A]] = Repetition(this, None, Some(max))
+  def +                      : Parser[Seq[A]] = this*>=1
 }
 
 object Parser {
@@ -137,6 +147,40 @@ object Parser {
       } else {
         Success(None, r.index, r.length)
       }
+    }
+  }
+
+  case class Repetition[+A](body: Parser[A], min: Option[Int], max: Option[Int]) extends Parser[Seq[A]] {
+    (min, max) match {
+      case (Some(a), Some(b)) if a > b => throw new IllegalArgumentException(s"Provided range [${min.get}, ${max.get}] is invalid.")
+      case (Some(x), _) if x < 0 => throw new IllegalArgumentException("Provided min must be a non-negative integer.")
+      case (_, Some(x)) if x < 0 => throw new IllegalArgumentException("Provided max must be a non-negative integer.")
+    }
+
+    private val minCount = min.getOrElse(0)
+    private val maxCount = max.getOrElse(-1)
+
+    def parse(context: ParsingContext): ParsingResult[Seq[A]] = {
+      val mark = context.mark
+      val products = collection.mutable.ListBuffer[A]()
+
+      var n = 0
+      while(true) {
+        val iterationContext = context.clone
+        val iterationResult = body.parse(iterationContext)
+
+        if(iterationResult.succeeded) {
+          context.assimilate(iterationContext)
+          products.append(iterationResult.product)
+          n += 1
+
+          if(maxCount == n)
+            return mark.success(products.toList)
+        } else {
+          return if(minCount > n) Failure else mark.success(products.toList)
+        }
+      }
+      ???
     }
   }
 
