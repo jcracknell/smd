306d8a1d9b31f7bb5b5a242df773dc67d0576b34
James Cracknell <james.h.cracknell@gmail.com>
2013-08-27 20:47:49 -0600

    Added smd.parsing.AndPredicateParser

---
+++ smd/src/main/scala/parsing/AndPredicateParser.scala
@@ -0,0 +1,7 @@
+package smd
+package parsing
+
+case class AndPredicateParser[+A](body: Parser[A]) extends Parser[A] {
+  def parse(context: ParsingContext): ParsingResult[A] =
+    body.parse(context.clone)
+}
--- smd/src/main/scala/parsing/Parsers.scala
+++ smd/src/main/scala/parsing/Parsers.scala
@@ -3,4 +3,6 @@ package parsing
 
 trait Parsers {
   implicit def string2LiteralParser(str: String): LiteralParser = LiteralParser(str)
+
+  def &:[A](parser: Parser[A]): AndPredicateParser[A] = AndPredicateParser(parser)
 }
