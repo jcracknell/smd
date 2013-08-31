28b4e9570c67199790bc54df4999a3c0df811826
James Cracknell <james.h.cracknell@gmail.com>
2013-08-31 14:06:54 -0600

    Added RichSeq

--- smd/src/main/scala/package.scala
+++ smd/src/main/scala/package.scala
@@ -3,6 +3,15 @@ package object smd {
     def proxySubSequence(start: Int, end: Int): CharSequence =
       new smd.util.ProxyCharSequence(cs, start, end)
   }
+
+  implicit class RichSeq[A](val seq: collection.Seq[A]) extends AnyVal {
+    def lengthEq(len: Int): Boolean  = seq.lengthCompare(len) == 0
+    def lengthGt(len: Int): Boolean  = seq.lengthCompare(len) >  0
+    def lengthGte(len: Int): Boolean = seq.lengthCompare(len) >= 0
+    def lengthLt(len: Int): Boolean  = seq.lengthCompare(len) <  0
+    def lengthLte(len: Int): Boolean = seq.lengthCompare(len) <= 0
+  }
+
   implicit class RichString(val s: String) extends AnyVal {
     def literalEncode: String = util.LiteralEncoding.encode(s)
   }
