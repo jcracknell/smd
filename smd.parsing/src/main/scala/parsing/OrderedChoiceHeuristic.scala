fe69f6dbf404d527202f636abee8f7e703aacfcf
James Cracknell <james.h.cracknell@gmail.com>
2013-09-01 22:01:28 -0600

    Tightened tparam bounds on smd.parsing.OrderedChoiceHeuristic

--- smd.parsing/src/main/scala/parsing/OrderedChoiceHeuristic.scala
+++ smd.parsing/src/main/scala/parsing/OrderedChoiceHeuristic.scala
@@ -1,12 +1,12 @@
 package smd
 package parsing
 
-trait OrderedChoiceHeuristic[-L <: Parser[_], -R, +C <: Parser[_]] extends ((L, R) => C) {
+trait OrderedChoiceHeuristic[-L <: Parser[_], -R <: Parser[_], +C <: Parser[_]] extends ((L, R) => C) {
   def apply(lhs: L, rhs: R): C
 }
 
 object OrderedChoiceHeuristic {
-  def create[L <: Parser[_], R, C <: Parser[_]](comb: (L, R) => C): OrderedChoiceHeuristic[L, R, C] =
+  def create[L <: Parser[_], R <: Parser[_], C <: Parser[_]](comb: (L, R) => C): OrderedChoiceHeuristic[L, R, C] =
     new OrderedChoiceHeuristic[L, R, C] {
       def apply(lhs: L, rhs: R): C = comb(lhs, rhs)
     }
