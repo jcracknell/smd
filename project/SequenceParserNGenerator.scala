2f4678b754897e11a4239cd31b60c63042c3ca04
James Cracknell <james.h.cracknell@gmail.com>
2013-09-02 21:04:05 -0600

    Parser sequencing now commutative

--- project/SequenceParserNGenerator.scala
+++ project/SequenceParserNGenerator.scala
@@ -8,9 +8,7 @@ class SequenceParserNGenerator(n: Int, maxN: Int) extends FileGenerator(_/"parsi
     x + IndexedSeq("th","st","nd","rd","th")(if(10 <= x && x <= 19) 4 else math.min(4, x % 10))
   }
 
-  private val types =            (1 to n).map("T"+_)
-  private val typeList =         types.mkString(", ")
-  private val productTupleType = s"($typeList)"
+  private def lst(prefix: String, is: Iterable[Int], suffix: String = ""): String = is.map(prefix+_+suffix).mkString(", ")
 
   def content: String =
     s"""
@@ -22,47 +20,58 @@ class SequenceParserNGenerator(n: Int, maxN: Int) extends FileGenerator(_/"parsi
     |
     |/** Parses $n expressions, providing strongly-typed results.
     |  * ${(1 to n).map({ i => s"""
-    |  * @param  _${i.toString.padTo(2, ' ')} the ${ordinal(i)} parser in the sequence.
+    |  * @param  p${i.toString.padTo(2, ' ')} the ${ordinal(i)} parser in the sequence.
     |  * @tparam T${i.toString.padTo(2, ' ')} the product type of the ${ordinal(i)} parser in the sequence.""".stripMargin }).mkString("")}
     |  */
-    |case class ${className()}[${types.map("+"+_).mkString(", ")}](
-    |  ${(1 to n).map(i => s"_$i: Parser[T$i]").mkString(", ")}
-    |) extends AnyRef with Parser[$productTupleType]
+    |case class ${className()}[${lst("+T", 1 to n)}](
+    |  ${(1 to n).map(i => s"p$i: Parser[T$i]").mkString(", ")}
+    |) extends AnyRef with Parser[(${lst("T", 1 to n)})]
     |{
-    |  def parse(context: ParsingContext): ParsingResult[$productTupleType] = {
+    |  def parse(context: ParsingContext): ParsingResult[(${lst("T", 1 to n)})] = {
     |    val rb = context.resultBuilder
     |
     |    ${(1 to n).map({ i => s"""
-    |    val r$i = _$i.parse(context)
+    |    val r$i = p$i.parse(context)
     |    if(r$i.failed) return rb.failure
     |    """.trim.stripMargin }).mkString("\n").trim}
     |
-    |    rb.success((${(1 to n).map(i => s"r$i.product").mkString(", ")}))
+    |    rb.success((${lst("r", 1 to n, ".product")}))
     |  }
     |}
     |
     |object SequenceParser$n {
     |$sequencingHeuristic
+    |${(2 to maxN-n).map({ i => s"""
+    |
+    |  /** Implicit [[smd.parsing.SequencingHeuristic]] which describes how to combine an [[${qualifiedClassName(n)}]]
+    |    * on the left-hand side with an [[${qualifiedClassName(i)}]] to its right. */
+    |  implicit def sequencingHeuristic_${n}_${i}[${lst("L", 1 to n)}, ${lst("R", 1 to i)}]: SequencingHeuristic[
+    |    /*  left: */ ${className(n)}[${lst("L", 1 to n)}],
+    |    /* right: */ ${className(i)}[${lst("R", 1 to i)}],
+    |    /*  dest: */ SequenceParser${n+i}[${lst("L", 1 to n)}, ${lst("R", 1 to i)}]
+    |  ] =
+    |    SequencingHeuristic.create((l, r) => ${className(n+i)}(${lst("l.p", 1 to n)}, ${lst("r.p", 1 to i)}))
+    |""".trim.stripMargin }).mkString("")}
     |}
     """.trim.stripMargin
 
   def sequencingHeuristic: String =
     if(n == maxN) s"""
-    |  implicit def sequencingHeuristic[$typeList, A]: SequencingHeuristic[
-    |    /*  left: */ ${className()}[$typeList],
-    |    /* right: */ Parser[A],
+    |  implicit def sequencingHeuristic[${lst("L", 1 to n)}, R]: SequencingHeuristic[
+    |    /*  left: */ ${className(n)}[${lst("L", 1 to n)}],
+    |    /* right: */ Parser[R],
     |    /*  dest: */ SequenceParser
     |  ] =
-    |    SequencingHeuristic.create((l, r) => SequenceParser(${(1 to n).map("l._"+_).mkString(", ")}, r))
+    |    SequencingHeuristic.create((l, r) => SequenceParser(${lst("l.p", 1 to n)}, r))
     """.trim.stripMargin
     else s"""
-    |  /** Implicit [[smd.parsing.SequencingHeuristic]] which can be used to combine an [[${qualifiedClassName()}]]
+    |  /** Implicit [[smd.parsing.SequencingHeuristic]] which describes how to combine an [[${qualifiedClassName()}]]
     |    * and an [[smd.parsing.Parser]] into an [[${qualifiedClassName(n+1)}]]. */
-    |  implicit def sequencingHeuristic[$typeList, A]: SequencingHeuristic[
-    |    /*  left: */ ${className()}[$typeList],
-    |    /* right: */ Parser[A],
-    |    /*  dest: */ ${className(n+1)}[$typeList, A]
+    |  implicit def sequencingHeuristic[${lst("L", 1 to n)}, R]: SequencingHeuristic[
+    |    /*  left: */ ${className(n)}[${lst("L", 1 to n)}],
+    |    /* right: */ Parser[R],
+    |    /*  dest: */ ${className(n+1)}[${lst("L", 1 to n)}, R]
     |  ] =
-    |    SequencingHeuristic.create((l, r) => ${className(n+1)}(${(1 to n).map("l._"+_).mkString(", ")}, r))
+    |    SequencingHeuristic.create((l, r) => ${className(n+1)}(${lst("l.p", 1 to n)}, r))
     """.trim.stripMargin
 }
