bca5416efcb9ba7a4d58addccfbcfb17871661a6
James Cracknell <james.h.cracknell@gmail.com>
2013-09-03 20:28:04 -0600

    smd.parsing.RegexParser checks grapheme boundaries

--- smd.parsing/src/main/scala/parsing/RegexParser.scala
+++ smd.parsing/src/main/scala/parsing/RegexParser.scala
@@ -8,12 +8,21 @@ case class RegexParser(regex: Regex) extends Parser[Regex.Match] {
   def parse(context: ParsingContext): ParsingResult[Match] = {
     // TODO: Verify this is our most performant option for matching at a specific index
     val rb = context.resultBuilder
+
     val remainingInput = context.input.proxySubSequence(context.index, context.input.length)
     val matchResult = regex.findPrefixMatchOf(remainingInput)
-    if(matchResult.isDefined) {
-      // TODO: Need to verify that the last grapheme was matched in its entirety
-      context.advanceBy(matchResult.get.end)
-      rb.success(matchResult.get)
+
+    if(matchResult.isEmpty)
+      return rb.failure
+
+    val m = matchResult.get
+
+    // The match must either be empty or finish a grapheme
+    // Note that as we are prefix matching against a subsequence, m.end is the length of the match
+    val matchEnd = context.index + m.end
+    if(matchEnd == context.index || matchEnd == context.graphemeAt(matchEnd - 1).end) {
+      context.advanceTo(matchEnd)
+      rb.success(m)
     } else {
       rb.failure
     }
