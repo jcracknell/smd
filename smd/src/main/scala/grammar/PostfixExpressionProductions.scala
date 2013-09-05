689e4ae84b85ba7cfae48cb58579d2b641e396e1
James Cracknell <james.h.cracknell@gmail.com>
2013-09-04 22:33:29 -0600

    Simplified PostfixExpression grammar

--- smd/src/main/scala/grammar/PostfixExpressionProductions.scala
+++ smd/src/main/scala/grammar/PostfixExpressionProductions.scala
@@ -4,8 +4,8 @@ package grammar
 trait PostfixExpressionProductions extends LeftHandSideExpressionProductions {
   lazy val PostfixExpression: Parser[Expression] =
     LeftHandSideExpression ~ ( ExpressionWhitespaceNoNewline ~>
-      "--" >>>>((body: Expression) => $ex.PostfixDecrement(body)) |
-      "++" >>>>((body: Expression) => $ex.PostfixIncrement(body))
+      "--" >>>> $ex.PostfixDecrement |
+      "++" >>>> $ex.PostfixIncrement
     ).? >>> {
       case (body, Some(builder)) => builder(body)
       case (body, _) => body
