8044d85827060612f8aef0cb0705ba921a13d865
James Cracknell <james.h.cracknell@gmail.com>
2013-09-10 18:47:26 -0600

    Tidied PrimaryExpressionProductions

--- smd/src/main/scala/smd/grammar/PrimaryExpressionProductions.scala
+++ smd/src/main/scala/smd/grammar/PrimaryExpressionProductions.scala
@@ -6,32 +6,48 @@ trait PrimaryExpressionProductions extends LiteralExpressionProductions
                                       with IdentifierExpressionProductions
 {
   /** A literal, array literal, or object literal expression. */
-  lazy val primaryExpression: Parser[Expression] =
-    literalExpression |
-    arrayLiteralExpression |
-    objectLiteralExpression |
-    "(" ~ expressionWhitespace ~> <>(expr) <~ expressionWhitespace ~ ")"
+  lazy val primaryExpression: Parser[Expression] = (
+    literalExpression
+  | arrayLiteralExpression
+  | objectLiteralExpression
+  | "(" ~ expressionWhitespace ~> <>(expr) <~ expressionWhitespace ~ ")"
+  )
 
-  lazy val arrayLiteralExpression =
-    "[" ~ expressionWhitespace ~ arrayElements ~ expressionWhitespace ~ "]" ^* { p => expression.ArrayLiteral(p._3) }
+  lazy val arrayLiteralExpression: Parser[expression.ArrayLiteral] = {
+    /** A non-elided array element preceded by any number of elided elements. */
+    val subsequentArrayElement =
+      argumentSeparator.+ ~ expressionWhitespace ~ <>(expr) ^* { case (seps, _, e) => seps.tail.map(_ => expression.Elided()) :+ e }
 
-  private lazy val arrayElements =
+    val arrayElements = (
+      (
+        <>(expr) ~ subsequentArrayElement.* ^* { case (e, ses) => e +: ses.flatten }
+      | subsequentArrayElement.+            ^* { p => expression.Elided() +: p.flatten } // initial element elided
+      ).?
+    <~ argumentSeparator.*
+    ^*(_.getOrElse(Seq()))
+    )
+
+    "[" ~ expressionWhitespace ~> arrayElements <~ expressionWhitespace ~ "]" ^* expression.ArrayLiteral
+  }
+
+  lazy val objectLiteralExpression: Parser[expression.ObjectLiteral] = {
+    val objectPropertyAssignment = (
+      (stringLiteral | numericLiteral ^*(_.toString) | identifier)
+    ~ expressionWhitespace ~ ":" ~ expressionWhitespace
+    ~ <>(expr)
+    ^* { p => (p._1, p._5) }
+    )
+
+    val objectPropertyAssignments = (
+      objectPropertyAssignment
+    ~ (argumentSeparator ~ objectPropertyAssignment).*
+    ~ argumentSeparator.?
+    ^* { p => p._1 +: p._2.map(_._2) }
+    )
+
     (
-      <>(expr) ~ subsequentArrayElement.* ^* { case (e, ses) => e +: ses.flatten } |
-      subsequentArrayElement.+                  ^* { p => expression.Elided() +: p.flatten } // initial element elided
-    ).? ~ argumentSeparator.* ^*(_._1.getOrElse(Seq()))
-
-  /** A non-elided array element preceded by any number of elided elements. */
-  private lazy val subsequentArrayElement =
-    argumentSeparator.+ ~ expressionWhitespace ~ <>(expr) ^* { case (seps, _, e) => seps.tail.map(_ => expression.Elided()) :+ e }
-
-  lazy val objectLiteralExpression =
-    "{" ~ expressionWhitespace ~ objectPropertyAssignments.? ~ expressionWhitespace ~ "}" ^* { case (_,_,ps,_,_) => expression.ObjectLiteral(ps.getOrElse(Seq())) }
-
-  private lazy val objectPropertyAssignments =
-    (objectPropertyAssignment ~ (argumentSeparator ~ objectPropertyAssignment).* ~ argumentSeparator.?) ^* { p => p._1 +: p._2.map(_._2) }
-
-  private lazy val objectPropertyAssignment =
-    (stringLiteral | numericLiteral ^*(_.toString) | identifier) ~
-    expressionWhitespace ~ ":" ~ expressionWhitespace ~ <>(expr) ^* { p => (p._1, p._5) }
+      "{" ~ expressionWhitespace ~> objectPropertyAssignments.? <~ expressionWhitespace ~ "}"
+    ^*{ ps => expression.ObjectLiteral(ps.getOrElse(Seq())) }
+    )
+  }
 }
