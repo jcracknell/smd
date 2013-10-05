4312192471ca51269abfea4fac217b5b95413d8d
James Cracknell <james.h.cracknell@gmail.com>
2013-10-05 14:11:37 -0600

    Fixed RepetitionParser constructor assertions

--- smd.parsing/src/main/scala/smd/parsing/RepetitionParser.scala
+++ smd.parsing/src/main/scala/smd/parsing/RepetitionParser.scala
@@ -2,13 +2,12 @@ package smd
 package parsing
 
 case class RepetitionParser[+A](body: Parser[A], min: Option[Int], max: Option[Int]) extends Parser[Seq[A]] {
-  (min, max) match {
-    case (Some(a), Some(b)) => require(a <= b, s"Provided range [${min.get}, ${max.get}] is invalid.")
-                               require(1 < b, "Provided max must be a positive integer.")
-    case (Some(x), _      ) => require(0 < x, "Provided min must be a non-negative integer.")
-    case (_,       Some(x)) => require(1 < x, "Provided max must be a positive integer.")
-    case _ =>
-  }
+  if(min.isDefined && max.isDefined)
+    assert(min.get <= max.get, s"Provided range [${min.get}, ${max.get}] is invalid.")
+  if(min.isDefined)
+    assert(min.get >= 0, "Provided min must be a non-negative integer.")
+  if(max.isDefined)
+    assert(max.get >= 1, "Provided max must be a positive integer.")
 
   private val minCount = min.getOrElse(0)
   private val maxCount = max.getOrElse(-1)
