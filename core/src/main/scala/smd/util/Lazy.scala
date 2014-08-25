833789e827bbfc813803f1ff511e7ce28f55a6f0
James Cracknell <james.h.cracknell@gmail.com>
2014-08-24 23:54:17 -0600

    Added extractor for smd.util.Lazy

--- core/src/main/scala/smd/util/Lazy.scala
+++ core/src/main/scala/smd/util/Lazy.scala
@@ -1,8 +1,13 @@
 package smd
 package util
 
+/** Trait explicitly encapsulating a lazily computed value. */
 trait Lazy[+A] {
+  /** Retrieves the lazily computed value of this instance, forcing evaluation. */
   def get: A
+
+  /** Maps the value of this instance to a new instance without forcing evaluation. Evaluation
+    * of the resulting instance will force evaluation. */
   def map[B](f: A => B): Lazy[B]
 }
 
@@ -26,8 +31,11 @@ object Lazy {
 
     def map[B](f: A => B): Lazy[B] = Lazy(f(get))
 
-    override def toString: String = if(unevaluated) "Lazy(???)" else s"Lazy($value)"
+    override def toString: String = if(unevaluated) "Lazy(?)" else s"Lazy($value)"
   }
+
+  /** Extractor for [[smd.util.Lazy]] instances. Pattern matching will force evaluation. */
+  def unapply[A](lzy: Lazy[A]): Option[A] = Some(lzy.get)
 
   /** Wraps the provided function such that its results are lazily evaluated. */
   def mapping[A, B](f: A => B): A => Lazy[B] = ((a: A) => Lazy(f(a)))
